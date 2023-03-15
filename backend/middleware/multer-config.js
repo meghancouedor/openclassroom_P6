b00509baab5033fb0b867f0ceb6367c60bc158e4
Meghan Couedor <meghan.couedor@gmail.com>
2023-03-15 21:00:50 +0100

    Configuration de Multer

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+
+const mimeTypes = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+
+//On configure le chemin et le nom de fichier pour les fichiers entrants
+const storage = multer.diskStorage({
+    //On indique d'enregistrer les fichiers dans le dossier Images
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = mimeTypes[file.mimetype];
+    //Création du file name
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+//Exportation du middleware
+module.exports = multer({ storage: storage }).single("image");
