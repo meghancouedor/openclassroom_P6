78d28efe42a6f54d29eefb5cc335fa9012fdeebd
Meghan Couedor <meghan.couedor@gmail.com>
2023-03-16 15:31:35 +0100

    Ajout CORS

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,22 @@ mongoose
 //Importation des routes
 const userRoutes = require("./routes/user");
 
+//CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.json());
+
 //Enregistrement des routes
 app.use("/api/auth", userRoutes);
 
